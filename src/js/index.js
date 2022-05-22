9d03ec19661c7401be999b0f9d9e2b090119ad18
razerfoot25 <jasonaldueza@gmail.com>
2022-05-23 05:27:31 +0800

    3rd commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,19 @@
 const menu = document.querySelector(".nav__menu--container");
 const menuIcon = document.querySelector("#menu__icon");
-const testClass = document.querySelector(".testClass");
-document.body.style.overflowX = "hidden";
+const testClass = document.querySelector(".nav__menu--bg");
+
 const toggleMenu = () => {
   menu.classList.toggle("menu-open");
 };
+
+const hideNav = () => {
+  if (!menu.classList.contains("menu-open")) {
+    testClass.style.display = "none";
+  }
+};
+
 menuIcon.addEventListener("click", (e) => {
   testClass.style.display = "block";
   setTimeout(toggleMenu, 100);
+  setTimeout(hideNav, 300);
 });
